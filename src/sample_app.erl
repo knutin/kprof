e079930c0359c976d1e99f0e7ab609e8f088f6d4
Knut Nesheim <knutin@gmail.com>
2011-08-04 23:05:05 +0200

    Slighly more meaningful sample_app for generating data for testing the couchapp.

--- src/sample_app.erl
+++ src/sample_app.erl
@@ -11,35 +11,45 @@ t() ->
                   {rendering, {?MODULE, render_response, 0}},
                   {logging, {?MODULE, logging, 0}}],
 
-    IdentityF = fun ({?MODULE, http, [foobar]}) -> foobar;
+    IdentityF = fun ({?MODULE, http, [Seed]}) ->
+                        case Seed rem 5 of
+                            0 -> fast_call;
+                            _ -> other_call
+                        end;
                     (_)      -> undefined
                 end,
 
     kprof:start_trace([{tier_config, TierConfig},
                        {identity_f, IdentityF},
-                       {print_calls, true},
+                       {print_calls, false},
                        {stats_dumper, {couchdb, []}}]).
-                       
+r(0) ->
+    done;
+r(N) ->
+    run(N),
+    timer:sleep(100),
+    r(N-1).
 
-r(Seed) ->
+run(Seed) ->
     spawn(fun() ->
                   kprof:do_apply(?MODULE, webserver_handler, [Seed])
           end).
     
 
 webserver_handler(Seed) ->
-    random:seed(Seed, Seed, Seed),
-    http(foobar).
+    random:seed(Seed * Seed,
+                Seed * (Seed * 3),
+                Seed * Seed * Seed * Seed),
+    http(Seed).
 
 
 http(_Req) ->
-    timer:sleep(2 + random()),
     database(),
     logging(),
     render_response().
 
 database() ->
-    timer:sleep(20 + random()),
+    timer:sleep(50 + random()),
     ok.
 
 logging() ->
@@ -50,7 +60,4 @@ render_response() ->
     timer:sleep(10 + random()).
 
 random() ->
-    round(random:uniform() * 50).
-
-    
-    
+    round(random:uniform() * 100).
